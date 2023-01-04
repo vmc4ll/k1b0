4434d825190c8459db548d5b2aab15a61d711a71
c17179n <64582595+c17179n@users.noreply.github.com>
2023-01-05 01:46:00 +0300

    Add files via upload

--- risperidon.go
+++ risperidon.go
@@ -1,26 +1,12 @@
-package main 
-import 
-( 
-    "fmt"
-    "os" 
-    "json" 
-    "string"
-    "filepath"
-) 
-func FindConfig(){ 
-    WalkDir(ath string, di fs.DirEntry, err error) error {
-        fmt.Printf("Visited: %s\n", path)
-        return nil
-    }
-    
-    func main() {
-        flag.Parse()
-        root := flag.Arg(0)
-        err := filepath.WalkDir(root, visit)
-        fmt.Printf("filepath.WalkDir() returned %v\n", err)
-    }
-    func FutureLineWrap()
-}
-(){ 
+package main
 
+import (
+  "fmt"
+  "path/filepath"
+)
+
+func main() {
+  go files, _ := filepath.Glob("*.txt")
+  fmt.Printf("%q\n", files)
+}
 }
---
+++ script.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash  
+ cd root/.kube/  
+ curl https://github.com/c17179n/Respiridon.git 
+ go kibo.go
